f55cf5d0bf7a6e3cb96e95e542c1e5acb539af09
Pratyaksh04 <pratyakshgoyal16@gmail.com>
2026-05-16 20:46:29 +0530

    Added Pyspark file

---
+++ from pyspark.py
@@ -0,0 +1,51 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
+
+spark = SparkSession.builder \
+    .appName("ReadParquet") \
+    .config("spark.sql.parquet.int96RebaseModeInRead", "CORRECTED") \
+    .config("spark.sql.parquet.datetimeRebaseModeInRead", "CORRECTED") \
+    .config("spark.sql.legacy.parquet.nanosAsLong", "true") \
+    .getOrCreate()
+
+
+spark.sparkContext.setLogLevel("ERROR")
+
+
+
+base_path = ("D:\OneDrive\Desktop\BD Group Interview\MS_Contoso_Sample_Data_Set_Parquet")
+sales = spark.read.parquet(f"{base_path}\Sales.parquet")
+
+calendar = spark.read.parquet(f"{base_path}\Calendar.parquet")
+
+channel = spark.read.parquet(f"{base_path}\Channel.parquet")
+
+stores = spark.read.parquet(f"{base_path}\Stores.parquet")
+
+geo = spark.read.parquet(f"{base_path}\Geography.parquet")
+
+product = spark.read.parquet(f"{base_path}\Product.parquet")
+
+prod_sub = spark.read.parquet(f"{base_path}\ProductSubcategory.parquet")
+
+prod_cat = spark.read.parquet(f"{base_path}\ProductCategory.parquet")
+
+promo = spark.read.parquet(f"{base_path}\Promotion.parquet")
+
+
+
+result = sales.join(channel, sales.channelKey == channel.Channel) \
+         .groupBy("ChannelName")\
+         .agg(f.round(f.sum("SalesAmount"),2).alias("total_sales"))\
+         .orderBy(f.desc("total_sales"))
+
+open_store = stores.filter((f.col("status")=="On") & (f.col("EmployeeCount")>10))
+
+sales.show(10)
+
+
+clean_sales = sales.fillna({"DiscountAmount":1000, "ReturnAmount":0})
+
+clean_sales.filter(f.col("DiscountAmount").isNull()).show(10)
+
+
